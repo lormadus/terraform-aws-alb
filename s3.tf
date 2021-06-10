dd5df7c147e7270047c58dd71e9ad1b8763c5126
David Yoon <david.hyeshik@yahoo.com>
2021-06-10 10:14:09 +0900

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,3 +1,7 @@
+# ALB 접근 로그를 저장하기 위해 생성
+# 이름은 S3가 PaaS서비스이기 때문에 특수문자나 "_" 사용 안됨
+# 글로벌하게 유일한 이름으로 생성 필요 (이미 생성된 S3 버킷이 있으면 생성안됨)
+
 resource "aws_s3_bucket" "user30-s3" {
   bucket = "sk-user30-alb-log.com"
   policy = <<EOF
