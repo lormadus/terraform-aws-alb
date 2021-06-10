9a368c1b4a7385044b7c1a4a027cfea4e99ba5b2
David Yoon <david.hyeshik@yahoo.com>
2021-06-10 10:00:51 +0900

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,3 +1,6 @@
+# Auto Scaling을 하는 경우, EC2 Intance 들이 서로 다른 가용영역(Availability Zone)에 설치 되어야
+# 하기 때문에 Subnet도 각각 다른 AZ에 생성 필요
+
 resource "aws_subnet" "user30-subnet1" {
   vpc_id            = aws_vpc.user30-vpc.id
   availability_zone = "ap-southeast-1a"
