0b939e19af93f2c0f517bf6170e90a632132571c
David Yoon <david.hyeshik@yahoo.com>
2021-06-10 10:17:56 +0900

    Update and rename tg.tf to 18. tg.tf

--- 18. tg.tf
+++ 18. tg.tf
@@ -1,3 +1,4 @@
+# ALB에서 전달한 트래픽을 처리할 인스턴스 지정
 resource "aws_alb_target_group" "user30-alb-tg" {
   name     = "user30-alb-tg"
   port     = 80
@@ -14,12 +15,14 @@ resource "aws_alb_target_group" "user30-alb-tg" {
   tags = { Name = "User30 Target Group" }
 }
 
+# 웹서버1번 등록
 resource "aws_alb_target_group_attachment" "tg-web1" {
   target_group_arn 	= aws_alb_target_group.user30-alb-tg.arn
   target_id        	= aws_instance.user30-web1.id
   port             	= 80
 }
 
+#웹서버2번 등록
 resource "aws_alb_target_group_attachment" "tg-web2" {
   target_group_arn 	= aws_alb_target_group.user30-alb-tg.arn
   target_id        	= aws_instance.user30-web2.id
